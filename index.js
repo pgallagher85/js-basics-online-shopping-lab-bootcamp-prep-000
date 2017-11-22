1675533e5d747b54b6168b24c76dc983abc5e919
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-22 02:54:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,5 +67,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber = undefined) {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  } else {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${itemNumber}.`)
+    cart.length = 0
+    return cart
+  }
 }
