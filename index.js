6dca21e8542d61f2be1e8f7340ad3f496abf9cb2
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-22 03:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function placeOrder(cardNumber) {
     console.log(`Sorry, we don't have a credit card on file for you.`)
     return cart
   } else {
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart.length = 0
     return cart
   }
